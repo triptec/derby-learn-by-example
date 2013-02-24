ca38a5e59eae3486de2c7fdade3f1696b892d84c
Andreas Franzén <andreas.franzen@osynlig.se>
2013-02-24 23:10:41 +0100

    cleaned out some unused functions

--- components/reset/index.js
+++ components/reset/index.js
@@ -4,33 +4,7 @@ exports.init = function(model) {
 }
 
 exports.create = function(model, dom) {
-    // sorry but we need jquery, especially for ajax
-    //TODO: Fix searchPath
     if (!window.$) require('../../public/foundation/javascripts/jquery.js');
-}
-
-exports.usernameBlur = function(){
-    // check username registered
-    var model = this.model,
-        rootModel = model.parent().parent(),
-        q = rootModel.query('users').withUsername(model.get('username'));
-    rootModel.fetch(q, function(err, users) {
-        try {
-            if (err) throw new Error(err);
-            var userObj = users.at(0).get()
-            if (!userObj) {
-                throw new Error("Username not registered. Make sure you're using the same capitalization you used to register!");
-            } else {
-                model.set('errors.username', '');
-            }
-        } catch (err) {
-            model.set('errors.username', err.message);
-        }
-    });
-}
-
-exports.loginSubmit = function(e, el){
-    // TODO handle server-side login failure response message here, via model.set('errors.password',..)
 }
 
 exports.showPasswordReset = function() {
